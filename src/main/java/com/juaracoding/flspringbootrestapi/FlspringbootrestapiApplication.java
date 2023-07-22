d31cd30e6cd37b0326f49ff3ceeff738ef65b232
fajarseptian <fauziahlatifah13@gmail.com>
2023-07-22 13:41:35 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/flspringbootrestapi/FlspringbootrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.flspringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlspringbootrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlspringbootrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juaracoding/flspringbootrestapi/FlspringbootrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.flspringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlspringbootrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
